763258a793403e2d48ee51d877c886406b629836
micky <mickymolina@gmail.com>
2023-06-14 18:05:22 -0400

    Commit 3

--- src/test/java/pages/todoIst/MenuConfiguration.java
+++ src/test/java/pages/todoIst/MenuConfiguration.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 public class MenuConfiguration {
     public Button changePswButton = new Button(By.xpath("//a[@href=\"/app/settings/account/password\"]"));
-    public TextBox actualPswBox = new TextBox(By.id("element-28"));
+    public TextBox actualPswBox = new TextBox(By.xpath("//div[@class=\"_6c84b26d _2a3b75a1 _509a57b4 e5a9206f _50ba6b6b\"]//input[@autocomplete=\"off\"]"));
     public TextBox newPswBox = new TextBox(By.id("element-31"));
     public TextBox newPswBoxConfirm = new TextBox(By.id("element-34"));
     public Button savePasswordButton = new Button(By.xpath("//button[@type=\"submit\"]"));
